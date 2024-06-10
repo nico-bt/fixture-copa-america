1609f2b2a132310b144ff7d3d63367fc09712e69
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-06-10 02:39:07 -0300

    Another try for linkedin autopreview img with open graph file instead of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,6 @@ export const metadata: Metadata = {
   description: "Arma tu prediccion",
   authors: { name: "Nicolas Battaglia" },
   keywords: ["Next.js", "React", "JavaScript", "Copa America"],
-  other: {
-    ["og:image"]: "https://fixture-copa-america.vercel.app/img/front.png",
-  },
 }
 
 export default function RootLayout({
